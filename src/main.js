2db63105cada89b887aa06f83c2308758f83e28b
vishwaroopa <vishwarupa.vishu@gmail.com>
2022-05-19 15:18:13 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import './index.css'
 export default function (Vue, { router, head, isClient }) {
   
   // Set default layout as a global component
-  if (window){
+   if(process.client) {
      let username = document.cookie.indexOf('analytics=');
         if (username != -1) {
             window['ga-disable-UA-100388653-1'] = true; // enter your tracking ID
